d9544d34d7a92095cd1a0c6f74dc4ec438d1ccfd
Eray Demircioğlu <c.eray@hotmail.com>
2023-12-23 13:45:27 +0300

    Code to Image

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="flex items-center gap-10 py-16">
+        <span className="flex items-center text-sm font-medium pr-2">&copy;2023. All rights reserved.</span>
+    </div>
+  );
+}
+
+export default Footer;
